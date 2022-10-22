b5506b19dc12bb25ecf64c23cbd442ebb32c8068
DrantDumani <darnjames@gmail.com>
2022-10-22 10:47:39 -0400

    Added gameLoop and gameLoop tests

---
+++ src/gameLoop.js
@@ -0,0 +1,50 @@
+function initGameLoop(gameBoardFn, shipFn, playerFn) {
+  let gameState = "setup";
+
+  const getGameState = () => gameState;
+  const startGame = () => {
+    gameState = "playing";
+  };
+
+  const p1Board = gameBoardFn();
+  const p2Board = gameBoardFn();
+  const player1 = playerFn("Player 1");
+  const player2 = playerFn("CPU");
+  let winner = null;
+
+  const getWinner = () => winner;
+
+  const shipLengths = [5, 4, 3, 3, 2];
+  const setupBoards = () => {
+    p1Board.randomize(shipLengths, shipFn);
+    p2Board.randomize(shipLengths, shipFn);
+  };
+
+  const takeTurn = (plyr, board, index) => {
+    board.receiveAttack(index);
+    if (board.allShipsSunk()) {
+      winner = plyr.type;
+      gameState = "game over";
+    }
+  };
+
+  const takeP1Turn = (index) => {
+    takeTurn(player1, p2Board, index);
+  };
+
+  const takeCPUTurn = (index) => {
+    takeTurn(player2, p1Board, index);
+  };
+  return {
+    p1Board,
+    p2Board,
+    getGameState,
+    startGame,
+    getWinner,
+    setupBoards,
+    takeP1Turn,
+    takeCPUTurn
+  };
+}
+
+export default initGameLoop;
